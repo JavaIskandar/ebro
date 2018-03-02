f94102c1cbdca14f011a6863eb9fedc9f8ffb6f7
Unknown <iskandarjava@yahoo.co.id>
2018-03-02 08:40:01 +0700

    model dan migrasi

--- database/migrations/2018_03_01_122048_create_cities_table.php
+++ database/migrations/2018_03_01_122048_create_cities_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateCitiesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,21 +13,17 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('cities', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('address');
-            $table->integer('id_city')
+            $table->timestamps();
+            $table->integer('province_id')
                 ->unsigned();
-            $table->foreign('id_city')
+            $table->foreign('province_id')
                 ->references('id')
-                ->on('cities')
+                ->on('provinces')
                 ->onUpdate('CASCADE')
                 ->onDelete('CASCADE');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('name');
         });
     }
 
@@ -38,6 +34,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('cities');
     }
 }
---
+++ database/migrations/2018_03_01_174727_create_stocks_table.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStocksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stocks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('name')->unique();
+            $table->integer('id_color')
+                ->unsigned();
+            $table->foreign('id_color')
+                ->references('id')
+                ->on('colors')
+                ->onUpdate('CASCADE')
+                ->onDelete('CASCADE');
+            $table->integer('id_size')
+                ->unsigned();
+            $table->foreign('id_size')
+                ->references('id')
+                ->on('sizes')
+                ->onUpdate('CASCADE')
+                ->onDelete('CASCADE');
+            $table->integer('id_material')
+                ->unsigned();
+            $table->foreign('id_material')
+                ->references('id')
+                ->on('materials')
+                ->onUpdate('CASCADE')
+                ->onUpdate('CASCADE');
+            $table->integer('id_gender')
+                ->unsigned();
+            $table->foreign('id_gender')
+                ->references('id')
+                ->on('genders')
+                ->onUpdate('CASCADE')
+                ->onDelete('CASCADE');
+            $table->integer('count');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stocks');
+    }
+}
