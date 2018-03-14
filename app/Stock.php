3f1c2f5309926ddc4ab1fd814cf33dcba3770320
Unknown <iskandarjava@yahoo.co.id>
2018-03-14 11:37:23 +0700

    Update model

--- app/Stock.php
+++ app/Stock.php
@@ -6,5 +6,44 @@ use Illuminate\Database\Eloquent\Model;
 
 class Stock extends Model
 {
-    //
+    protected $table = 'stocks';
+
+    protected $keyType = 'string';
+
+    public $incrementing = false;
+
+    protected $fillable = [
+        'name', 'id_type', 'id_color', 'id_size', 'id_material', 'id_gender', 'long_sleeve', 'quantity'
+    ];
+
+    public function getRelationProduct()
+    {
+        return $this->hasMany('App\Product', 'id_product');
+    }
+
+    public function getProduct()
+    {
+        return $this->getRelationProduct()->get();
+    }
+
+    public function getRelationSize()
+    {
+        return $this->belongsTo('App\Size', 'id_size');
+    }
+
+    public function getSize()
+    {
+        return $this->getRelationSize()->get();
+    }
+
+    public function getRelationType()
+    {
+        return $this->belongsTo('App\Type', 'id_type');
+    }
+
+    public function getType()
+    {
+        return $this->getRelationType()->get();
+    }
+
 }
--- app/Type.php
+++ app/Type.php
@@ -6,5 +6,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class Type extends Model
 {
-    //
+    protected $table = 'types';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'name', 'id_category'
+    ];
+
+    public function getStock(bool $queryReturn = false)
+    {
+        $stock = $this->hasMany('App\Stock', 'id_stock');
+        if ($queryReturn)
+            return $stock;
+        return $stock->get();
+    }
+
+
+    public function getProduct(bool $queryReturn = false)
+    {
+        $all_stock_id = $this->getStock(true)->select('id')->get()->toArray();
+        $all_stock_id = array_flatten($all_stock_id);
+        $product = Product::whereIn('id_stock', $all_stock_id);
+        if ($queryReturn)
+            return $product;
+        return $product->get();
+    }
 }
