575201235396c55747460ce40e464b0014e9c8e1
Unknown <nissacahya@gmail.com>
2018-03-14 20:26:00 +0700

    update create

--- resources/views/layouts/appCreate.blade.php
+++ resources/views/layouts/appCreate.blade.php
@@ -57,7 +57,7 @@
             <div class="row">
                 <div class="col-sm-4">
                     <div class="logo pull-left">
-                        <a><img src="{{ asset('storage/design/logoe&.png') }}"alt="" /></a>
+                        <a><img src="{{ asset('storage/design/logo e&bro.png') }}"alt="" /></a>
                     </div>
                 </div>
                 <div class="col-sm-8">
